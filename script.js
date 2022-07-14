71505fcf560b4fd7f0bdca696f3fe3b86bc7f092
Juliana Fernandes <julianasfernandes@hotmail.com>
2022-07-14 14:46:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+(function(){
+    'use strict';
+    var $form = document.querySelector('form');
+    var $txtUser = document.getElementById('txtUser');
+    var $txtPass = document.getElementById('txtPass');
+    var $btn = document.getElementById('btn');
+    
+    $form.addEventListener('submit', function(e){  
+        if(!$txtUser.value || !$txtPass.value ){
+            alert('Preencha todos os campos');
+            e.preventDefault();
+        }
+    });            
+    
+    
+    
+    
+})()
